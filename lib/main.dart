1468fac7fadb5b1836db1b1306940cd553288590
FajarWahyuSuryaputra <fsuryaputra028@gmail.com>
2025-01-10 19:12:54 +0700

    dashboard ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:course_travel/features/destination/presentation/cubit/dashboard_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +11,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => DashboardCubit()),
+      ],
+      child: const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: Text('Hello World!'),
+          ),
         ),
       ),
     );
