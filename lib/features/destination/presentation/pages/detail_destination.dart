9f26c2e733363ce2457b01bc83bfce888f107c41
FajarWahyuSuryaputra <fsuryaputra028@gmail.com>
2025-01-19 19:06:55 +0700

    gallery detail page

--- lib/features/destination/presentation/pages/detail_destination.dart
+++ lib/features/destination/presentation/pages/detail_destination.dart
@@ -1,5 +1,12 @@
+import 'package:course_travel/api/urls.dart';
 import 'package:course_travel/features/destination/domain/entities/DestinationEntity.dart';
+import 'package:course_travel/features/destination/presentation/widget/gallery_photo.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_grid_view.dart';
+import 'package:staggered_grid_view_flutter/widgets/staggered_tile.dart';
+
+import '../widget/circle_loading.dart';
 
 class DetailDestination extends StatefulWidget {
   const DetailDestination({super.key, required this.destination});
@@ -13,11 +20,112 @@ class _DetailDestinationState extends State<DetailDestination> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: appBar(),
+      appBar: header(),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        children: [
+          SizedBox(
+            height: 10,
+          ),
+          gallery(),
+        ],
+      ),
     );
   }
 
-  AppBar appBar() {
+  Widget gallery() {
+    List patternGallery = [
+      StaggeredTile.count(3, 3),
+      StaggeredTile.count(2, 1.5),
+      StaggeredTile.count(2, 1.5),
+    ];
+    return StaggeredGridView.countBuilder(
+      crossAxisSpacing: 16,
+      mainAxisSpacing: 16,
+      crossAxisCount: 5,
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: 3,
+      staggeredTileBuilder: (index) {
+        return patternGallery[index % patternGallery.length];
+      },
+      itemBuilder: (context, index) {
+        if (index == 2) {
+          return GestureDetector(
+            onTap: () {
+              showDialog(
+                  context: context,
+                  builder: (context) =>
+                      GalleryPhoto(images: widget.destination.images));
+            },
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  itemGalleryImage(index),
+                  Container(
+                    color: Colors.black.withOpacity(0.3),
+                    alignment: Alignment.center,
+                    child: Text(
+                      '+ More',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        }
+        return ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: itemGalleryImage(index));
+      },
+    );
+  }
+
+  ClipRRect itemGalleryImage(int index) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(12),
+      child: ExtendedImage.network(
+        URLs.image(widget.destination.images[index]),
+        fit: BoxFit.cover,
+        handleLoadingProgress: true,
+        loadStateChanged: (state) {
+          if (state.extendedImageLoadState == LoadState.failed) {
+            return AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Material(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.grey[300],
+                child: const Icon(
+                  Icons.broken_image,
+                  color: Colors.black,
+                ),
+              ),
+            );
+          }
+          if (state.extendedImageLoadState == LoadState.loading) {
+            return AspectRatio(
+              aspectRatio: 16 / 9,
+              child: Material(
+                borderRadius: BorderRadius.circular(16),
+                color: Colors.grey[300],
+                child: const CircleLoading(),
+              ),
+            );
+          }
+          return null;
+        },
+      ),
+    );
+  }
+
+  AppBar header() {
     return AppBar(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       title: Text(
